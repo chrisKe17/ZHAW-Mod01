888554b9fb12a1b8cb827dad867e06ef660882ac
ChrisTEKO <christof.keller@edu.teko.ch>
2024-02-24 10:23:03 +0100

    Anpassung Aufgaben Modul 02 Zinsberechnung

--- src/InterestCalc.java
+++ src/InterestCalc.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 public class InterestCalc {
     public static void main(String[] args) {
-        //Variablen
-        float kontostand = 2000;
+        System.out.println("Welche Laufzeit: ");
+        Scanner scanner = new Scanner(System.in); //scanner initalisieren und befüllen
+        int duration = scanner.nextInt();
+
+        System.out.println("Welcher Betrag: ");
+        scanner = new Scanner(System.in); //Variabel scanner erneut befüllen, resp. überschreiben
+        float kontostand = scanner.nextInt();
+
         float rate = 2;
-        int duration = 10;
-        float kontostandNeu;
-        float interrest;
 
         for (int i = 1; i <= duration; i++) {
-            interrest = kontostand / 100 * rate;
-            kontostandNeu = kontostand + interrest;
+            float interrest = kontostand / 100 * rate;
+            float kontostandNeu = kontostand + interrest;
             System.out.println("Kontostand im " + i + ". Jahr ist " +kontostandNeu );
             kontostand = kontostandNeu;
         }
--- src/SpeedCalc.java
+++ src/SpeedCalc.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class SpeedCalc {
     public static void main(String[] args) {
-        //Variablen
-        float speed =0;
-        float totalDist;
-        float reactDist = 0;
-        float stoppDist =0;
 
         Scanner eingabe1 = new Scanner(System.in);
         System.out.println("Geben Sie die Geschwindigkeit in km/h ein: ");
-        speed = eingabe1.nextFloat();
+        float speed = eingabe1.nextFloat();
 
-        reactDist = 3*(speed/10);
-        stoppDist = (speed/10)*(speed/10);
-        totalDist = reactDist + stoppDist;
+        float reactDist = 3*(speed/10);
+        float stoppDist = (speed/10)*(speed/10);
+        float totalDist = reactDist + stoppDist;
 
-        System.out.println("totalDist in m = " + totalDist);
-        System.out.println("stoppDist in m = " + stoppDist);
-        System.out.println("reactDist in m = " + reactDist);
+        System.out.println("TotalDist in m = " + totalDist);
+        System.out.println("StoppDist in m = " + stoppDist);
+        System.out.println("ReactDist in m = " + reactDist);
 
     }
 }
